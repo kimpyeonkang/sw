95173272ff4fa2ad56b1427971a8402efc551fd6
김평강 <129609893+kimpyeonkang@users.noreply.github.com>
2023-07-08 14:57:29 +0900

    Add files via upload

---
+++ 2023.07.07/달팽이 배열.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#define size 5
+int main(void)
+{
+	int arry[50][50];
+	int i, j, idx;
+	int w = -1, s = 0, inc = 1, val = 0;
+
+	idx = size;
+
+	while (1)
+	{
+		for (i = 0; i < idx; i++) // 가로 단위 그림
+		{
+			val++;
+			w = w + inc;
+			arry[s][w] = val;
+		}
+		idx--;
+
+		if (val == size * size)
+			break;
+
+		for (i = 0; i < idx; i++) // 세로 단위 그림
+		{
+			val++;
+			s = s + inc;
+			arry[s][w] = val;
+		}
+		inc *= -1;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			printf("%2d ", arry[i][j]);
+		printf("\n");
+	}
+
+	return 0;
+}
